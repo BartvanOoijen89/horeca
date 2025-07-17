2c14ef971ad51e8464a02dab423cc1a5e2360b01
BartvanOoijen89 <bart@appeltern.nl>
2025-07-17 15:49:08 +0200

    Update voorspelling_app.py

--- voorspelling_app.py
+++ voorspelling_app.py
@@ -137,6 +137,74 @@ def get_weer_voor_dag(datum):
         bron = "OpenWeather (forecast)"
     return temp, neerslag, bron
 
+# ---- FUNCTIE: voorspelling per groep én product ----
+
+def voorspelling_per_groep_en_product(begroot, temp, neerslag, datum_sel, locaties):
+    groep_totaal = {groep: 0 for groep in PRODUCTGROEPEN}
+    producten_per_groep = {groep: [] for groep in PRODUCTGROEPEN}
+    totaal = 0
+    for omzetgroep in PRODUCTGROEPEN:
+        df_p = df_aggr[
+            (df_aggr['datum'] < datum_sel) &
+            (df_aggr['omzetgroep naam'] == omzetgroep) &
+            (df_aggr['locatie'].isin(locaties))
+        ]
+        if len(df_p) < 3:
+            continue
+        df_p = pd.merge(df_p, bezoekers_df[['datum', 'begroot aantal bezoekers']], on='datum', how='left')
+        df_p = pd.merge(df_p, weerdata, on='datum', how='left')
+        df_p = df_p.dropna(subset=['begroot aantal bezoekers', 'Temp', 'Neerslag', 'aantal'])
+        if len(df_p) < 3:
+            continue
+        producten = df_p['product name'].unique()
+        for product in producten:
+            df_prod = df_p[df_p['product name'] == product]
+            if len(df_prod) < 3:
+                continue
+            X = df_prod[['begroot aantal bezoekers', 'Temp', 'Neerslag']]
+            y = df_prod['aantal']
+            model = LinearRegression().fit(X, y)
+            x_voorspel = pd.DataFrame({'begroot aantal bezoekers': [begroot], 'Temp': [temp], 'Neerslag': [neerslag]})
+            aantal = int(round(model.predict(x_voorspel)[0]))
+            aantal = max(0, aantal)
+            if aantal > 0:
+                producten_per_groep[omzetgroep].append((product, aantal))
+                groep_totaal[omzetgroep] += aantal
+                totaal += aantal
+    return groep_totaal, producten_per_groep, totaal
+
+# ---- START UI ----
+
+st.markdown(f"""
+# Park horeca omzet- & verkoopvoorspelling
+
+### {datum_sel.strftime('%d-%m-%Y')}
+""")
+
+col1, col2, col3 = st.columns(3)
+
+# Begroot, Voorspeld, Werkelijk aantal bezoekers
+bezoek = bezoekers_df[bezoekers_df['datum'] == datum_sel]
+begroot = int(bezoek['begroot aantal bezoekers'].iloc[0]) if not bezoek.empty else 0
+if not bezoek.empty and 'totaal aantal bezoekers' in bezoek.columns and pd.notna(bezoek['totaal aantal bezoekers'].iloc[0]):
+    werkelijk = int(bezoek['totaal aantal bezoekers'].iloc[0])
+else:
+    werkelijk = 0
+voorspeld = begroot  # Eenvoudig, model hier toepassen als je wilt
+
+col1.metric("Begroot aantal bezoekers", begroot)
+col2.metric("Voorspeld aantal bezoekers", voorspeld)
+col3.metric("Werkelijk aantal bezoekers", werkelijk)
+
+# Weersvoorspelling tonen
+temp, neerslag, weer_bron = get_weer_voor_dag(datum_sel)
+st.markdown(f"""
+<div style='background-color:#314259; padding: 1em; border-radius: 8px; color:#fff'>
+<b>Weersvoorspelling:</b> max temp {temp:.1f}°C, neerslag {neerslag:.1f} mm<br>
+<i>bron: {weer_bron}</i>
+</div>
+""", unsafe_allow_html=True)
+
 # ---- PRODUCTVOORSPELLING ----
 
 st.markdown("## Voorspeld aantal verkochte producten (per productgroep):")
@@ -193,65 +261,6 @@ for groep in PRODUCTGROEPEN:
             table_html += f"<tr><td>{product}</td><td>{a}</td></tr>"
         table_html += "</table>"
         st.markdown(table_html, unsafe_allow_html=True)
-
-st.markdown(
-    f"<b>Totaal voorspelde verkoop (bovenstaande groepen): {totaal_voorspeld}</b>",
-    unsafe_allow_html=True
-)
-
-# ---- START UI ----
-
-st.markdown(f"""
-# Park horeca omzet- & verkoopvoorspelling
-
-### {datum_sel.strftime('%d-%m-%Y')}
-""")
-
-col1, col2, col3 = st.columns(3)
-
-# Begroot, Voorspeld, Werkelijk aantal bezoekers
-bezoek = bezoekers_df[bezoekers_df['datum'] == datum_sel]
-begroot = int(bezoek['begroot aantal bezoekers'].iloc[0]) if not bezoek.empty else 0
-if not bezoek.empty and 'totaal aantal bezoekers' in bezoek.columns and pd.notna(bezoek['totaal aantal bezoekers'].iloc[0]):
-    werkelijk = int(bezoek['totaal aantal bezoekers'].iloc[0])
-else:
-    werkelijk = 0
-voorspeld = begroot  # Eenvoudig, model hier toepassen als je wilt
-
-col1.metric("Begroot aantal bezoekers", begroot)
-col2.metric("Voorspeld aantal bezoekers", voorspeld)
-col3.metric("Werkelijk aantal bezoekers", werkelijk)
-
-# Weersvoorspelling tonen
-temp, neerslag, weer_bron = get_weer_voor_dag(datum_sel)
-st.markdown(f"""
-<div style='background-color:#314259; padding: 1em; border-radius: 8px; color:#fff'>
-<b>Weersvoorspelling:</b> max temp {temp:.1f}°C, neerslag {neerslag:.1f} mm<br>
-<i>bron: {weer_bron}</i>
-</div>
-""", unsafe_allow_html=True)
-
-# ---- PRODUCTVOORSPELLING ----
-
-st.markdown("## Voorspeld aantal verkochte producten (per productgroep):")
-
-groep_totaal, producten_per_groep, totaal_voorspeld = voorspelling_per_groep_en_product(
-    begroot, temp, neerslag, datum_sel, gekozen_locaties_data
-)
-
-for groep in PRODUCTGROEPEN:
-    aantal = groep_totaal[groep]
-    if aantal > 0:
-        st.markdown(
-            f"<span style='font-size:1.08em; font-weight:bold; color:#314259'>{groep}: {aantal} stuks</span>",
-            unsafe_allow_html=True
-        )
-        table_html = "<table style='margin-left:2em;font-size:1em;'>"
-        for product, a in producten_per_groep[groep]:
-            table_html += f"<tr><td style='padding-right:1em;'>{product}:</td><td style='font-weight:600;'>{a}</td></tr>"
-        table_html += "</table>"
-        st.markdown(table_html, unsafe_allow_html=True)
-        st.markdown("")  # witregel
 
 st.markdown(
     f"<b>Totaal voorspelde verkoop (bovenstaande groepen): {totaal_voorspeld}</b>",
